0eff5c6330b32ccd913115076c1e96a80d38a4ee
mostefa <21nounou21@gmail.com>
2021-06-08 19:16:24 +0100

    added comments

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-
-        File file =
-                new File("src/weapon.txt");
+        //open text file using File class and Scanner class
+        File file = new File("src/weapon.txt");
                 Scanner sc = new Scanner(file);
-int i =0;
+                //initializing a counter with 0
+                int i =0;
+                //loop that runs until the end of file (EoF)
         while (sc.hasNextLine()){
-           String line =  sc.nextLine();
-           //if(line.equals("Boule (Pointe,Chaude,Bleu,Pleine,12 cm) Boule (Pointe,Chaude,Bleu,Pleine,12 cm)"))i++;
+            //get first line in file
+            String line =  sc.nextLine();
+            //split it on two parts because in each line there are two art pieces and we need to check each one seperately
             String[] splitLine = line.split("\\) ");
+            //added a closing parentheses to the first part to return it to the original state which is Forme (info,info,info,info,info)
             splitLine[0]=splitLine[0]+")";
+            //test if art peice corresponds to the wanted one, if it is we increment the counter
             if(splitLine[0].equals("Boule (Pointe,Chaude,Bleu,Pleine,12 cm)"))i++;
             if(splitLine[1].equals("Boule (Pointe,Chaude,Bleu,Pleine,12 cm)"))i++;
         }
+        //printing the counter
         System.out.println("murder weapon number : "+i);
     }
 }
